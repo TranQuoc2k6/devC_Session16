99195ff6f9b3d9302886b292afa49150e6b8e040
TranQuoc2k6 <qatran2k6@gmail.com>
2024-12-16 01:44:59 +0700

    Add files via upload

---
+++ Session16-6.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int searchElement(int *arr, int size, int value) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == value) {
+            return i;
+        }
+    }
+    return -1;
+}
+int main() {
+    int arr[] = {1, 7, 8, 3, 8, 9};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int value = 3;
+    printf("Input: [");
+    for (int i = 0; i < size; i++) {
+        printf("%d", arr[i]);
+        if (i < size - 1) {
+            printf(", ");
+        }
+    }
+    printf("]\n");
+    int index = searchElement(arr, size, value);
+    if (index != -1) {
+        printf("Phan tu %d duoc tim thay tai vi tri: %d\n", value, index);
+    } else {
+        printf("Phan tu %d khong tim thay trong mang\n", value);
+    }
+    return 0;
+}
